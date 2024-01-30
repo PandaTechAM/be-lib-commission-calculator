f5fe0f986f37204e105ebd2cde0c120a6ed7d597
Haik <haik.asatran@pandatech.it>
2024-01-30 21:32:08 +0400

    Added overlap detection static class

---
+++ src/CommissionCalculator/Helper/DateTimeOverlapChecker.cs
@@ -0,0 +1,27 @@
+﻿using CommissionCalculator.DTO;
+
+namespace CommissionCalculator.Helper;
+
+public static class DateTimeOverlapChecker
+{
+    public static bool HasOverlap(List<DateTimePair> firstPairs, List<DateTimePair> secondPairs)
+    {
+        foreach (var firstPair in firstPairs)
+        {
+            foreach (var secondPair in secondPairs)
+            {
+                if (IsOverlapping(firstPair, secondPair))
+                {
+                    return true;
+                }
+            }
+        }
+        return false; 
+    }
+
+    private static bool IsOverlapping(DateTimePair firstPair, DateTimePair secondPair)
+    {
+        
+        return firstPair.StartDate <= secondPair.EndDate && secondPair.StartDate <= firstPair.EndDate;
+    }
+}
